023d654343d234186c54e5a910e47485c28303fc
Umang Agarwal <umangagarwal257@gmail.com>
2025-04-08 20:58:28 +0530

    Add class 4 : vault-anchor

--- solana-starter/ts/cluster1/nft_mint.ts
+++ solana-starter/ts/cluster1/nft_mint.ts
@@ -16,14 +16,14 @@ umi.use(mplTokenMetadata())
 const mint = generateSigner(umi);
 
 (async () => {
-    let tx = await createNft(umi, {mint, name : "IPL NFT Collection", symbol : "CSK" , uri : "https://devnet.irys.xyz/Eo4GdN1magNuxnXD2jkJjbSgJgLvHbts1oPcvMaBzfGC", sellerFeeBasisPoints : percentAmount(4)})
+    let tx = await createNft(umi, { mint, name: "IPL NFT Collection", symbol: "CSK", uri: "https://devnet.irys.xyz/Eo4GdN1magNuxnXD2jkJjbSgJgLvHbts1oPcvMaBzfGC", sellerFeeBasisPoints: percentAmount(4) })
     let result = await tx.sendAndConfirm(umi);
     const signature = base58.encode(result.signature);
-    
+
     console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
 
     console.log("Mint Address: ", mint.publicKey);
 })();
 
 // mint address = HVRbA1upEN22TiiM87kDLwKV6gkjJcS55vAEiuH26ZFM
-// 7GpEnYoM9U6TiPaFK7JEubaVVRW1ogtm9Q45sMVoH2Lw
+//                7GpEnYoM9U6TiPaFK7JEubaVVRW1ogtm9Q45sMVoH2Lw
