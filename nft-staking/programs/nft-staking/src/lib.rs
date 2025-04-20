2181a9f72fd80bff2dccad127a48eb3266c51d21
Umang Agarwal <umangagarwal257@gmail.com>
2025-04-20 17:07:29 +0530

    nft-staking init

---
+++ nft-staking/programs/nft-staking/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("4tymRt4zeN4fpggEY27P9FeyScPBCmfDez49nFVQ8DbE");
+
+#[program]
+pub mod nft_staking {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
