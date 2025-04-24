99d0027a67a257a1f8572823aaf445d9932eb01a
Umang Agarwal <umangagarwal257@gmail.com>
2025-04-24 21:55:19 +0530

    added escrow test

--- escrow/programs/escrow/src/instructions/take.rs
+++ escrow/programs/escrow/src/instructions/take.rs
@@ -1,3 +1,4 @@
+#![allow(unexpected_cfgs)]
 use crate::state::Escrow;
 use anchor_lang::prelude::*;
 use anchor_spl::{
--- escrow/tests/escrow.ts
+++ escrow/tests/escrow.ts
@@ -1,245 +1,211 @@
-import { before, describe, test, it } from "node:test";
-import assert from "node:assert";
-import * as programClient from "../dist/js-client";
-import { connect, Connection, SOL, TOKEN_EXTENSIONS_PROGRAM } from "solana-kite";
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { SYSTEM_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/native/system";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  createMint,
+  getAssociatedTokenAddress,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+  TOKEN_PROGRAM_ID
+} from "@solana/spl-token";
+import {
+  Keypair,
+  PublicKey,
+  sendAndConfirmTransaction,
+  Transaction
+} from "@solana/web3.js";
+import { BN } from "bn.js";
+import { Escrow } from "../target/types/escrow";
 
-const SYSTEM_PROGRAM = "11111111111111111111111111111111" as Address;
+const secretKey = Uint8Array.from(require("../keys/turbin3.json"));
+const keypair = Keypair.fromSecretKey(secretKey);
 
-// For debugging. You could delete these, but then someone else will have to recreate them and then they'll be annoyed with you.
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const log = console.log;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const stringify = (obj: any) => JSON.stringify(obj, null, 2);
+describe("escrow", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
 
-import { lamports, type KeyPairSigner, type Address } from "@solana/kit";
+  const connection = anchor.getProvider().connection;
 
-const ONE_SOL = lamports(1n * SOL);
+  const program = anchor.workspace.Escrow as Program<Escrow>;
 
-const getRandomBigInt = () => {
-  return BigInt(Math.floor(Math.random() * 1_000_000_000_000_000_000));
-};
+  let mintA;
+  let mintB;
 
-// See https://www.quicknode.com/guides/solana-development/tooling/web3-2/program-clients#generate-clients
-describe("Escrow", () => {
-  let connection: Connection;
-  let user: KeyPairSigner;
+  it("Is initialized!", async () => {
+    mintA = await createMint(connection, keypair, keypair.publicKey, null, 6);
+    mintB = await createMint(connection, keypair, keypair.publicKey, null, 6);
 
-  // Alice will be the maker (creator) of the offer
-  let alice: KeyPairSigner;
+    const ataA = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mintA,
+      keypair.publicKey
+    );
 
-  // Bob will be the taker (acceptor) of the offer
-  let bob: KeyPairSigner;
+    const ataB = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mintB,
+      keypair.publicKey
+    );
 
-  // tokenMintA represents the token Alice is offering
-  // tokenMintB represents the token Alice wants in return
-  let tokenMintA: Address;
-  let tokenMintB: Address;
+    await mintTo(
+      connection,
+      keypair,
+      mintA,
+      ataA.address,
+      keypair.publicKey,
+      10_000_000
+    );
 
-  let vault: Address;
-  let offer: Address;
+    await mintTo(
+      connection,
+      keypair,
+      mintB,
+      ataB.address,
+      keypair.publicKey,
+      10_000_000
+    );
 
-  let offerId: bigint;
+    const balanceA = await connection.getTokenAccountBalance(ataA.address);
+    const balanceB = await connection.getTokenAccountBalance(ataB.address);
 
-  let aliceTokenAccountA: Address;
-  let bobTokenAccountA: Address;
-  let aliceTokenAccountB: Address;
+    console.log("Initial balance A => ", balanceA.value.uiAmount);
+    console.log("Initial balance B => ", balanceB.value.uiAmount);
 
-  let tokenAOfferedAmount = 1_000_000_000n;
-  let tokenBWantedAmount = 1_000_000_000n;
+    const seed = new BN(1);
+    const receive = new BN(1_000_000);
 
-  // Create Alice and Bob accounts, 2 token mints, and associated token accounts for both tokens for both users
-  before(async () => {
-    connection = await connect();
+    const [escrow] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("escrow"),
+        keypair.publicKey.toBuffer(),
+        Buffer.from(seed.toArray("le", 8))
+      ],
+      program.programId
+    );
 
-    // This is the user that will pay for the transactions to create the token mints
-    [user, alice, bob] = await connection.createWallets(3, { airdropAmount: ONE_SOL });
+    const vault = await getAssociatedTokenAddress(mintA, escrow, true);
 
-    tokenMintA = await connection.createTokenMint({
-      mintAuthority: user,
-      decimals: 9,
-      name: "Token A",
-      symbol: "TOKEN_A",
-      uri: "https://example.com/token-a",
-      additionalMetadata: {
-        keyOne: "valueOne",
-        keyTwo: "valueTwo",
-      },
+    const makeIx = await program.methods
+      .make(seed, receive)
+      .accounts({
+        maker: keypair.publicKey,
+        mintA: mintA,
+        mintB: mintB,
+        // @ts-ignore
+        makerAtaA: ataA.address,
+        escrow,
+        vaultA: vault,
+        associatedTokenProgam: ASSOCIATED_TOKEN_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systeProgram: SYSTEM_PROGRAM_ID
+      })
+      .instruction();
+
+    // const depositAmount = new BN(1_000_000);
+
+    // const makeIx = await program.methods
+    //   .make(depositAmount)
+    //   .accounts({
+    //     maker: keypair.publicKey,
+    //     mintA: mintA,
+    //     mintB: mintB,
+    //     // @ts-ignore
+    //     makerAtaA: ataA.address,
+    //     escrow,
+    //     vaultA: vault,
+    //     associatedTokenProgam: ASSOCIATED_TOKEN_PROGRAM_ID,
+    //     tokenProgram: TOKEN_PROGRAM_ID,
+    //     systeProgram: SYSTEM_PROGRAM_ID
+    //   })
+    //   .instruction();
+
+    const tx = new Transaction().add(makeIx);
+    tx.feePayer = keypair.publicKey;
+
+    const txHash = await sendAndConfirmTransaction(connection, tx, [keypair], {
+      skipPreflight: true
     });
 
-    tokenMintB = await connection.createTokenMint({
-      mintAuthority: user,
-      decimals: 9,
-      name: "Token B",
-      symbol: "TOKEN_B",
-      uri: "https://example.com/token-b",
-      additionalMetadata: {
-        keyOne: "valueOne",
-        keyTwo: "valueTwo",
-      },
-    });
+    console.log("Your transaction signature", txHash);
 
-    // Alice will have 2n * tokenAOfferedAmount of token A and 0 of token B
-    // 2n * tokenAOfferedAmount because Alice will make two offers, and each offer will have tokenAOfferedAmount of token A
-    // the first will be taken, and the second will be refunded.
-    await connection.mintTokens(tokenMintA, user, 2n * tokenAOfferedAmount, alice.address);
+    const vaultBalanceA = await connection.getTokenAccountBalance(vault);
+    // const balanceB = await connection.getTokenAccountBalance(ataB.address);
 
-    // Get Alice's token A account
-    aliceTokenAccountA = await connection.getTokenAccountAddress(alice.address, tokenMintA, true);
+    console.log("Vault balance A => ", vaultBalanceA.value.uiAmount);
+    // console.log("Initial balance B => ", balanceB);
 
-    // Bob will have 0 of token A and 1_000_000_000 of token B
-    // he will use the Token B to take Alice's offer.
-    await connection.mintTokens(tokenMintB, user, 1_000_000_000n, bob.address);
+    const balanceAafterDeposit = await connection.getTokenAccountBalance(
+      ataA.address
+    );
+    // const balanceB = await connection.getTokenAccountBalance(ataB.address);
+
+    console.log(
+      "balance A after deposit => ",
+      balanceAafterDeposit.value.uiAmount
+    );
+    // console.log("balance B => ", balanceB);
   });
 
-  test("Puts the tokens Alice offers into the vault when Alice makes an offer", async () => {
-    offerId = getRandomBigInt();
+  it("Refunded!", async () => {
+    const ataA = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mintA,
+      keypair.publicKey
+    );
 
-    // Get Bob's token A account (which may not exist yet)
-    bobTokenAccountA = await connection.getTokenAccountAddress(bob.address, tokenMintA, true);
+    const ataB = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mintB,
+      keypair.publicKey
+    );
 
-    // Get Alice's token B account (which may not exist yet)
-    aliceTokenAccountB = await connection.getTokenAccountAddress(alice.address, tokenMintB, true);
+    const seed = new BN(1);
 
-    // Derive the offer PDA
-    const offerPDAAndBump = await connection.getPDAAndBump(programClient.ESCROW_PROGRAM_ADDRESS, [
-      "offer",
-      alice.address,
-      offerId,
-    ]);
+    const [escrow] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("escrow"),
+        keypair.publicKey.toBuffer(),
+        Buffer.from(seed.toArray("le", 8))
+      ],
+      program.programId
+    );
 
-    offer = offerPDAAndBump.pda;
+    const vault = await getAssociatedTokenAddress(mintA, escrow, true);
 
-    // Derive the vault PDA (which will be an Associated Token Account)
-    vault = await connection.getTokenAccountAddress(offer, tokenMintA, true);
+    const refundIx = await program.methods
+      .refund(seed)
+      .accounts({
+        maker: keypair.publicKey,
+        mintA: mintA,
+        mintB: mintB,
+        // @ts-ignore
+        makerAtaA: ataA.address,
+        escrow,
+        vaultA: vault,
+        associatedTokenProgam: ASSOCIATED_TOKEN_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systeProgram: SYSTEM_PROGRAM_ID
+      })
+      .instruction();
 
-    const makeOfferInstruction = await programClient.getMakeOfferInstructionAsync({
-      maker: alice,
-      tokenMintA,
-      tokenMintB,
-      makerTokenAccountA: aliceTokenAccountA,
-      offer,
-      vault,
-      id: offerId,
-      tokenAOfferedAmount,
-      tokenBWantedAmount,
-      tokenProgram: TOKEN_EXTENSIONS_PROGRAM,
+    const tx = new Transaction().add(refundIx);
+    tx.feePayer = keypair.publicKey;
+
+    const txHash = await sendAndConfirmTransaction(connection, tx, [keypair], {
+      skipPreflight: true
     });
 
-    const transactionSignature = await connection.sendTransactionFromInstructions({
-      feePayer: alice,
-      instructions: [makeOfferInstruction],
-    });
-  });
+    console.log("Your transaction signature", txHash);
 
-  test("Puts the tokens from the vault into Bob's account, and gives Alice Bob's tokens, when Bob takes an offer", async () => {
-    const takeOfferInstruction = await programClient.getTakeOfferInstructionAsync({
-      taker: bob,
-      maker: alice.address,
-      tokenMintA,
-      tokenMintB,
-      takerTokenAccountA: bobTokenAccountA,
-      makerTokenAccountB: aliceTokenAccountB,
-      offer: offer,
-      vault: vault,
-      tokenProgram: TOKEN_EXTENSIONS_PROGRAM,
-    });
+    const balanceA = await connection.getTokenAccountBalance(ataA.address);
+    // const balanceB = await connection.getTokenAccountBalance(ataB.address);
 
-    const transactionSignature = await connection.sendTransactionFromInstructions({
-      feePayer: alice,
-      instructions: [takeOfferInstruction],
-    });
-
-    // Check the offered tokens are now in Bob's account
-    // (note: there is no before balance as Bob didn't have any offered tokens before the transaction)
-    const bobTokenAccountBalanceAfterResponse = await connection.getTokenAccountBalance({
-      tokenAccount: bobTokenAccountA,
-      mint: tokenMintA,
-      useTokenExtensions: true,
-    });
-
-    const bobTokenAccountBalanceAfter = bobTokenAccountBalanceAfterResponse.amount;
-    assert(bobTokenAccountBalanceAfter === tokenAOfferedAmount);
-
-    // Check the wanted tokens are now in Alice's account
-    // (note: there is no before balance as Alice didn't have any wanted tokens before the transaction)
-    const aliceTokenAccountBalanceAfterResponse = await connection.getTokenAccountBalance({
-      tokenAccount: aliceTokenAccountB,
-      mint: tokenMintB,
-      useTokenExtensions: true,
-    });
-
-    const aliceTokenAccountBalanceAfter = aliceTokenAccountBalanceAfterResponse.amount;
-    assert(aliceTokenAccountBalanceAfter === tokenBWantedAmount);
-  });
-
-  test("Returns tokens to Alice when she refunds her offer", async () => {
-    // We'll reuse the same token mints, but make a new offer and then refund it
-    // Create a new offer
-    const newOfferId = getRandomBigInt();
-    const newOfferPDAAndBump = await connection.getPDAAndBump(programClient.ESCROW_PROGRAM_ADDRESS, [
-      "offer",
-      alice.address,
-      newOfferId,
-    ]);
-    const newOffer = newOfferPDAAndBump.pda;
-    const newVault = await connection.getTokenAccountAddress(newOffer, tokenMintA, true);
-
-    const aliceSolBalance = await connection.getLamportBalance(alice.address);
-
-    // Make a new offer, using a new offerId and offer account
-
-    const makeOfferInstruction = await programClient.getMakeOfferInstructionAsync({
-      maker: alice,
-      tokenMintA,
-      tokenMintB,
-      makerTokenAccountA: aliceTokenAccountA,
-      offer: newOffer,
-      vault: newVault,
-      id: newOfferId,
-      tokenAOfferedAmount,
-      tokenBWantedAmount,
-      tokenProgram: TOKEN_EXTENSIONS_PROGRAM,
-    });
-    const transactionSignature = await connection.sendTransactionFromInstructions({
-      feePayer: alice,
-      instructions: [makeOfferInstruction],
-    });
-
-    // Get Alice's token balance before refund
-    const aliceTokenAccountBalanceBeforeResponse = await connection.getTokenAccountBalance({
-      tokenAccount: aliceTokenAccountA,
-      mint: tokenMintA,
-      useTokenExtensions: true,
-    });
-    const aliceTokenAccountBalanceBefore = aliceTokenAccountBalanceBeforeResponse.amount;
-    // Refund the offer
-    const refundOfferInstruction = await programClient.getRefundOfferInstructionAsync({
-      maker: alice,
-      tokenMintA,
-      makerTokenAccountA: aliceTokenAccountA,
-      offer: newOffer,
-      vault: newVault,
-      tokenProgram: TOKEN_EXTENSIONS_PROGRAM,
-    });
-    const refundTransactionSignature = await connection.sendTransactionFromInstructions({
-      feePayer: alice,
-      instructions: [refundOfferInstruction],
-    });
-
-    // Check tokens were returned to Alice
-    const aliceTokenAccountBalanceAfterResponse = await connection.getTokenAccountBalance({
-      wallet: alice.address,
-      mint: tokenMintA,
-      useTokenExtensions: true,
-    });
-    const aliceTokenAccountBalanceAfter = aliceTokenAccountBalanceAfterResponse.amount;
-    // Assert the balance is greater than the before balance
-    assert(aliceTokenAccountBalanceAfter > aliceTokenAccountBalanceBefore);
-    // Verify vault is closed
-    const isClosed = await connection.checkTokenAccountIsClosed({
-      tokenAccount: newVault,
-      useTokenExtensions: true,
-    });
-    assert(isClosed);
+    console.log("balance A after refund => ", balanceA.value.uiAmount);
+    // console.log("balance B => ", balanceB);
   });
 });
